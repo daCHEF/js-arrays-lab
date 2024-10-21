5a0272dd3c4b569b28180c75ff16b6993eb1d704
daCHEF <Doughboi85@gmail.com>
2024-10-21 00:00:21 -0400

    added 'taco' to string array

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-/*
-Exercise 1: Define an empty array
+/* Exercise 1: Define an empty array----------------------------------
 
 1) Create an empty array and assign it to a variable called `foods`.
 
@@ -11,8 +10,7 @@ Exercise 1 has been completed for you:
 // console.log('Exercise 1 result:', foods);
 
 
-/*
-Exercise 2: Add strings to the array
+/* Exercise 2: Add strings to the array-------------------------------
 
 1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
 
@@ -21,23 +19,23 @@ Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
 Complete Exercise 2 in the space below:
 */
 
-const foods = ['pizza', 'cheeseburger']; 
-    return 
+// const foods = ['pizza', 'cheeseburger'];
 
-console.log('Exercise 2 result:', foods);
+// console.log('Exercise 2 result:', foods);
 
 
-/*
-Exercise 3: Insert at the beginning
+/* Exercise 3: Insert at the beginning--------------------------------
 
 1) Insert the string 'taco' at the beginning of the `foods` array.
 
 Complete Exercise 3 in the space below:
 */
 
+const foods = ['pizza', 'cheeseburger'];
+foods.unshift('taco');
 
 
-// console.log('Exercise 3 result:', foods);
+console.log('Exercise 3 result:', foods);
 
 
 /*
