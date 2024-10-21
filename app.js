da69450b9cd5fa323b1ed653a764112f5ce29ba9
daCHEF <Doughboi85@gmail.com>
2024-10-21 01:46:34 -0400

    1) Use the 'join()' method to concatenate the strings in the 'foods' array, separated by ' -> '.
    2) Assign the result to a variable called 'allFoods'.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* Exercise 1: Define an empty array----------------------------------
 
-1) Create an empty array and assign it to a variable called `foods`.
+1) Create an empty array and assign it to a variable called 'foods'.
 
 Exercise 1 has been completed for you:
 */
@@ -12,7 +12,7 @@ Exercise 1 has been completed for you:
 
 /* Exercise 2: Add strings to the array-------------------------------
 
-1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
+1) Add 'pizza' and 'cheeseburger' to the 'foods' array. 
 
 Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
 
@@ -26,7 +26,7 @@ Complete Exercise 2 in the space below:
 
 /* Exercise 3: Insert at the beginning--------------------------------
 
-1) Insert the string 'taco' at the beginning of the `foods` array.
+1) Insert the string 'taco' at the beginning of the 'foods' array.
 
 Complete Exercise 3 in the space below:
 */
@@ -43,7 +43,7 @@ Complete Exercise 3 in the space below:
 1) Retrieve the 'pizza' string from the array based on its position (index) in
    the array.  
 
-2) Assign it to a variable called `favFood`.
+2) Assign it to a variable called 'favFood'.
 
 Complete Exercise 4 in the space below:
 */
@@ -69,7 +69,7 @@ Complete Exercise 5 in the space below:
 
 /* Exercise 6: Replace elements---------------------------------------
 
-1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+1) Replace 'pizza' in the 'foods' array with 'sushi' and 'cupcake'.
 
 Complete Exercise 6 in the space below:
 */
@@ -80,12 +80,12 @@ Complete Exercise 6 in the space below:
 // console.log('Exercise 6 result:', foods);
 
 
-/* Exercise 7: Using the `slice()` method-----------------------------
+/* Exercise 7: Using the 'slice()' method-----------------------------
 
 1) Use the 'slice()' method to create a new array that contains 'sushi' and 
    'cupcake'.
 
-2) Assign it to a variable named `yummy`.
+2) Assign it to a variable named 'yummy'.
 
 Complete Exercise 7 in the space below:
 */
@@ -98,27 +98,26 @@ Complete Exercise 7 in the space below:
 
 /* Exercise 8: Finding an index---------------------------------------
 
-1) Using the `indexOf()` method, find the index of the string 'tofu' in the 
-   `foods` array. 
+1) Using the 'indexOf()' method, find the index of the string 'tofu' in the 
+   'foods' array. 
 
-2) Assign it to a variable named `soyIdx`.
+2) Assign it to a variable named 'soyIdx'.
 
 Complete Exercise 8 in the space below:
 */
 
-const foods = ['pizza', 'glizzys', 'tofu', 'wings'];
-soyIdx = foods.indexOf('tofu');
+// const foods = ['pizza', 'glizzys', 'tofu', 'wings'];
+// soyIdx = foods.indexOf('tofu');
 
-console.log('Exercise 8 result:', soyIdx);
+// console.log('Exercise 8 result:', soyIdx);
 
 
-/*
-Exercise 9: Joining elements
+/* Exercise 9: Joining elements---------------------------------------
 
-1) Use the `join()` method to concatenate the strings in the `foods` array, 
+1) Use the 'join()' method to concatenate the strings in the 'foods' array, 
    separated by ' -> '. 
 
-2) Assign the result to a variable called `allFoods`. 
+2) Assign the result to a variable called 'allFoods'. 
 
 Note: The final result should log as:
 'taco -> sushi -> cupcake -> tofu -> cheeseburger'
@@ -126,18 +125,19 @@ Note: The final result should log as:
 Complete Exercise 9 in the space below:
 */
 
+const yummy = ['taco', 'sushi', 'cupcake', 'tofu', 'cheeseburger'];
+const allFoods = yummy.join(' -> ');
 
-
-// console.log('Exercise 9 result:', allFoods);
+console.log('Exercise 9 result:', allFoods);
 
 
 /*
 Exercise 10: Check for an element
 
-1) Using the .includes() method, check if the `foods` array contains the string
+1) Using the .includes() method, check if the 'foods' array contains the string
    'soup'.
 
-2) Assign the result to a variable called `hasSoup``.
+2) Assign the result to a variable called 'hasSoup''.
 
 Complete Exercise 10 in the space below:
 */
@@ -150,11 +150,11 @@ Complete Exercise 10 in the space below:
 /*
 Exercise 11: Odd numbers from an array
 
-1) Choose a method to iterate through the `nums` array.
+1) Choose a method to iterate through the 'nums' array.
 
-2) Push each odd number to a new array named `odds`.
+2) Push each odd number to a new array named 'odds'.
 
-Hint: Initialize the `odds` variable to an empty array before the iteration.
+Hint: Initialize the 'odds' variable to an empty array before the iteration.
 
 Complete Exercise 11 in the space below:
 */
@@ -169,18 +169,18 @@ Complete Exercise 11 in the space below:
 /*
 Exercise 12: FizzBuzz with arrays
 
-1) Choose a method to iterate through the `nums` array. 
+1) Choose a method to iterate through the 'nums' array. 
 
 2. As you loop, sort the numbers into new arrays based on the following rules:
 
-   - Push any number evenly divisible by 3 to an array called `fizz`.
-   - Push any number evenly divisible by 5 to an array called `buzz`.
+   - Push any number evenly divisible by 3 to an array called 'fizz'.
+   - Push any number evenly divisible by 5 to an array called 'buzz'.
    - Push any number that is evenly divisible by 3 and 5 to an array called
-     `fizzbuzz`.
+     'fizzbuzz'.
 
    Note: A single number may meet more than one of the above rules. If it does,
-         it should be placed in multiple arrays. For example, the number `15`
-         will appear in the `fizz`, `buzz`, and `fizzbuzz` arrays.
+         it should be placed in multiple arrays. For example, the number '15'
+         will appear in the 'fizz', 'buzz', and 'fizzbuzz' arrays.
 
 Complete Exercise 12 in the space below:
 */
@@ -196,11 +196,11 @@ Complete Exercise 12 in the space below:
 /*
 Exercise 13: Retrieve the Last Array
 
-1) Assign the last nested array in the `numArrays` below to a variable named
-   `numList`. As you do this, also fulfill these goals:
+1) Assign the last nested array in the 'numArrays' below to a variable named
+   'numList'. As you do this, also fulfill these goals:
 
-   - Assume you don't know how many nested arrays `numArrays` contains.
-   - Do not alter the original `numArrays` array.
+   - Assume you don't know how many nested arrays 'numArrays' contains.
+   - Do not alter the original 'numArrays' array.
 
 Complete Exercise 13 in the space below:
 */
@@ -218,10 +218,10 @@ Complete Exercise 13 in the space below:
 /*
 Exercise 14: Accessing within nested arrays
 
-1) Retrieve the number `66` from the `numArrays` array. As part of this process
-   do not alter the original `numArrays` array.
+1) Retrieve the number '66' from the 'numArrays' array. As part of this process
+   do not alter the original 'numArrays' array.
 
-2) Assign it to a variable called `num`.
+2) Assign it to a variable called 'num'.
 
 Complete Exercise 14 in the space below:
 */
@@ -234,10 +234,10 @@ Complete Exercise 14 in the space below:
 /*
 Exercise 15: Nested array sum
 
-1) Use nested loops or `forEach()` methods to sum up all numbers within 
-   `numArrays` nested arrays.
+1) Use nested loops or 'forEach()' methods to sum up all numbers within 
+   'numArrays' nested arrays.
    
-2) Assign the sum to a variable called `total`.
+2) Assign the sum to a variable called 'total'.
 
 Hint: Be sure to declare and initialize the total variable before the iterations.
 
